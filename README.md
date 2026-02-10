381b76c1a7a8e838fbad27e3ca35124738ce5b65
Chandler King <chandler.king@findheadway.com>
2026-02-10 13:52:54 -0500

    set up typer

---
+++ README.md
@@ -0,0 +1,3 @@
+# Headway Dev
+
+A CLI tool for initiating an isolated dev environment for working with AI agents. By using this tool, you can successfully leverage AI agents like Claude Code to work on multiple features and branches simultaneously!
--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from headway-dev!")
+from typing import Annotated
+
+import typer
+
+
+app = typer.Typer(
+    invoke_without_command=True,
+    no_args_is_help=True,
+    context_settings={"help_option_names": ["--help", "-h"]},
+)
+
+
+@app.command()
+def hello(
+    name: Annotated[str, typer.Argument()],
+    formal: Annotated[bool, typer.Option(help="use a formal tone")] = False,
+) -> None:
+    if formal:
+        print(f"Good day {name}")
+    else:
+        print(f"Hello {name}")
+
+
+@app.command()
+def goodbye(
+    name: Annotated[str, typer.Argument()],
+    formal: Annotated[bool, typer.Option(help="use a formal tone")] = False,
+) -> None:
+    if formal:
+        print(f"Good morrow {name}")
+    else:
+        print(f"See ya {name}!")
 
 
 if __name__ == "__main__":
-    main()
+    app()
